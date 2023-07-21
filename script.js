2cbcbada12b496d35d99244f51729648c68c7427
Murali Krishna <muralikanna77@gmail.com>
2023-07-21 19:25:48 +0530

    stage1

---
+++ script.js
@@ -0,0 +1,6 @@
+const main = document.querySelector("#main")
+const cursor = document.querySelector("#cursor")
+main.addEventListener("mousemove", function (dets) {
+    cursor.style.left = (dets.x + 20) + "px"
+    cursor.style.top = (dets.y + 20) + "px"
+})
